21f42ad2fc46ffeae31beda1c6672916e020cc51
Honza Dvorsky <honza@swiftkey.com>
2015-04-12 21:25:08 +0100

    rolled my own logging in the end

--- BuildaUtils/Persistence.swift
+++ BuildaUtils/Persistence.swift
@@ -9,9 +9,9 @@
 import Foundation
 import BuildaUtils
 
-class Persistence {
+public class Persistence {
     
-    class func loadJSONFromUrl(url: NSURL) -> (AnyObject?, NSError?) {
+    public class func loadJSONFromUrl(url: NSURL) -> (AnyObject?, NSError?) {
         
         var error: NSError?
         if let data = NSData(contentsOfURL: url, options: NSDataReadingOptions.allZeros, error: &error) {
@@ -23,7 +23,7 @@ class Persistence {
         return (nil, error)
     }
     
-    class func saveJSONToUrl(json: AnyObject, url: NSURL) -> (Bool, NSError?) {
+    public class func saveJSONToUrl(json: AnyObject, url: NSURL) -> (Bool, NSError?) {
         
         var error: NSError?
         if let data = NSJSONSerialization.dataWithJSONObject(json, options: NSJSONWritingOptions.PrettyPrinted, error: &error) {
@@ -35,7 +35,7 @@ class Persistence {
         return (false, error)
     }
     
-    class func getFileInAppSupportWithName(name: String, isDirectory: Bool) -> NSURL {
+    public class func getFileInAppSupportWithName(name: String, isDirectory: Bool) -> NSURL {
         
         let root = self.buildaApplicationSupportFolderURL()
         let url = root.URLByAppendingPathComponent(name, isDirectory: isDirectory)
@@ -45,7 +45,7 @@ class Persistence {
         return url
     }
         
-    class func createFolderIfNotExists(url: NSURL) {
+    public class func createFolderIfNotExists(url: NSURL) {
         
         let fm = NSFileManager.defaultManager()
         
@@ -54,7 +54,7 @@ class Persistence {
         assert(success, "Failed to create a folder in Builda's Application Support folder \(url), error \(error)")
     }
     
-    class func buildaApplicationSupportFolderURL() -> NSURL {
+    public class func buildaApplicationSupportFolderURL() -> NSURL {
         
         let fm = NSFileManager.defaultManager()
         if let appSupport = fm.URLsForDirectory(NSSearchPathDirectory.ApplicationSupportDirectory, inDomains:NSSearchPathDomainMask.UserDomainMask).first as? NSURL {
@@ -75,7 +75,7 @@ class Persistence {
         return NSURL()
     }
     
-    class func iterateThroughFilesInFolder(folderUrl: NSURL, visit: (url: NSURL) -> ()) {
+    public class func iterateThroughFilesInFolder(folderUrl: NSURL, visit: (url: NSURL) -> ()) {
         
         let fm = NSFileManager.defaultManager()
         var error: NSError?
--- Buildasaur/AppDelegate.swift
+++ Buildasaur/AppDelegate.swift
@@ -9,23 +9,42 @@
 import Cocoa
 
 /*
-TODO: Keychain: GitHub token
-
 Please report any crashes on GitHub, I may optionally ask you to email them to me. Thanks!
 You can find them at ~/Library/Logs/DiagnosticReports/Buildasaur-*
-
+Also, you can find the log at ~/Library/Application Support/Buildasaur/Builda.log
 */
 
-import BuildaCIServer
+import BuildaUtils
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    var server: XcodeServer?
-    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         
+        self.setupLogging()
+    }
+    
+    func setupLogging() {
+        
+        let path = Persistence.buildaApplicationSupportFolderURL().URLByAppendingPathComponent("Builda.log", isDirectory: false)
+        let fileLogger = FileLogger(filePath: path)
+        let consoleLogger = ConsoleLogger()
+        let loggers: [Logger] = [
+            consoleLogger,
+            fileLogger
+        ]
+        Log.addLoggers(loggers)
+        let version = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"] as! String
+        let ascii =
+        " ____        _ _     _\n" +
+        "|  _ \\      (_) |   | |\n" +
+        "| |_) |_   _ _| | __| | __ _ ___  __ _ _   _ _ __\n" +
+        "|  _ <| | | | | |/ _` |/ _` / __|/ _` | | | | '__|\n" +
+        "| |_) | |_| | | | (_| | (_| \\__ \\ (_| | |_| | |\n" +
+        "|____/ \\__,_|_|_|\\__,_|\\__,_|___/\\__,_|\\__,_|_|\n"
+        
+        Log.untouched("*\n*\n*\n\(ascii)\nBuildasaur \(version) launched at \(NSDate()).\n*\n*\n*\n")
     }
     
     func applicationWillTerminate(aNotification: NSNotification) {
