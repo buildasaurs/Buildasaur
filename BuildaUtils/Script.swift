e8affafda4dd06990b9444e432501ca944c0403d
Honza Dvorsky <honza@swiftkey.com>
2015-05-12 22:02:24 +0100

    added support for running scripts from Builda...

---
+++ BuildaUtils/Script.swift
@@ -0,0 +1,75 @@
+//
+//  Script.swift
+//  Buildasaur
+//
+//  Created by Honza Dvorsky on 12/05/15.
+//  Copyright (c) 2015 Honza Dvorsky. All rights reserved.
+//
+
+import Foundation
+
+public class Script {
+    
+    public typealias ScriptResponse = (terminationStatus: Int, standardOutput: String, standardError: String)
+    
+    //TODO: create a small project for this and github+pod it, I couldn't find a simple library for running
+    //terminal scripts from Mac apps in Swift. might be useful to other people.
+    public func run(name: String, arguments: [String]) -> ScriptResponse {
+        
+        //first resolve the name of the script to a path with `which`
+        let resolved = self.runResolved("/usr/bin/which", arguments: [name])
+        let path = resolved.standardOutput
+        
+        //if resolving failed, just abort and propagate the failed run up
+        if (resolved.terminationStatus != 0) || (count(path) == 0) {
+            return resolved
+        }
+        
+        //ok, we have a valid path, run the script
+        let result = self.runResolved(path, arguments: arguments)
+        return result
+    }
+    
+    private func runResolved(path: String, arguments: [String]) -> ScriptResponse {
+        
+        //find path of script "name" with which
+        if name == "/usr/bin"
+        let path =
+        let pid = NSProcessInfo.processInfo().processIdentifier
+        
+        let outputPipe = NSPipe()
+        let errorPipe = NSPipe()
+        
+        let outputFile = outputPipe.fileHandleForReading
+        let errorFile = errorPipe.fileHandleForReading
+        
+        let task = NSTask()
+        task.launchPath = path
+        task.arguments = arguments
+        task.standardOutput = pipe
+        task.standardError = pipe
+        
+        task.launch()
+        
+        //blocks until script finishes.
+        //TODO: think about edge cases and long running/hangings tasks - some sort of a timeout?
+        task.waitUntilExit()
+        
+        let data = file.readDataToEndOfFile()
+        file.closeFile()
+        
+        let output = NSString(data: data, encoding: NSUTF8StringEncoding)
+        println("output: \(output)")
+    }
+    
+    private func stringFromFileAndClose(file: NSFileHandle) -> String {
+        
+        let data = file.readDataToEndOfFile()
+        file.closeFile()
+        let output = NSString(data: data, encoding: NSUTF8StringEncoding) as String?
+        return output ?? ""
+    }
+    
+
+}
+
