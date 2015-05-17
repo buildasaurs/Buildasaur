a2c2486be09d144b405d48b8ca24cfe13fc435d8
Honza Dvorsky <honza@swiftkey.com>
2015-05-18 00:00:10 +0100

    more tests

--- BuildasaurTests/SyncPair_PR_Bot_Tests.swift
+++ BuildasaurTests/SyncPair_PR_Bot_Tests.swift
@@ -60,4 +60,102 @@ class SyncPair_PR_Bot_Tests: XCTestCase {
         XCTAssertEqual(actions.githubStatusToSet!.status.state, Status.State.Pending)
     }
     
+    func testMultipleIntegrationsPending() {
+        
+        let (pr, bot) = self.mockedPRAndBot()
+        let integrations = [
+            MockIntegration(number: 1, step: Integration.Step.Pending),
+            MockIntegration(number: 2, step: Integration.Step.Pending),
+            MockIntegration(number: 3, step: Integration.Step.Pending)
+        ]
+        
+        let actions = SyncPair_PR_Bot.syncPRWithBotIntegrations(pr: pr, bot: bot, integrations: integrations)
+        
+        //should cancel all except for the last one
+        let toCancel = Set(actions.integrationsToCancel!)
+        XCTAssertEqual(toCancel.count, 2)
+        XCTAssertTrue(toCancel.contains(integrations[0]))
+        XCTAssertTrue(toCancel.contains(integrations[1]))
+        XCTAssertFalse(actions.startNewIntegration ?? true)
+        XCTBAssertNotNil(actions.githubStatusToSet)
+        XCTAssertEqual(actions.githubStatusToSet!.status.state, Status.State.Pending)
+    }
+    
+    func testOneIntegrationRunning() {
+        
+        let (pr, bot) = self.mockedPRAndBot()
+        let integrations = [
+            MockIntegration(number: 1, step: Integration.Step.Building),
+        ]
+        
+        let actions = SyncPair_PR_Bot.syncPRWithBotIntegrations(pr: pr, bot: bot, integrations: integrations)
+        
+        XCTAssertEqual(actions.integrationsToCancel!.count, 0)
+        XCTAssertFalse(actions.startNewIntegration ?? true)
+        XCTBAssertNotNil(actions.githubStatusToSet)
+        XCTAssertEqual(actions.githubStatusToSet!.status.state, Status.State.Pending)
+    }
+    
+    func testOneIntegrationTestsFailed() {
+        
+        let (pr, bot) = self.mockedPRAndBot()
+        let integrations = [
+            MockIntegration(number: 1, step: Integration.Step.Completed, sha: "head_sha", result: Integration.Result.TestFailures)
+        ]
+        
+        let actions = SyncPair_PR_Bot.syncPRWithBotIntegrations(pr: pr, bot: bot, integrations: integrations)
+        
+        XCTAssertEqual(actions.integrationsToCancel!.count, 0)
+        XCTAssertFalse(actions.startNewIntegration ?? true)
+        XCTBAssertNotNil(actions.githubStatusToSet)
+        XCTAssertEqual(actions.githubStatusToSet!.status.state, Status.State.Failure)
+    }
+    
+    func testOneIntegrationSuccess() {
+        
+        let (pr, bot) = self.mockedPRAndBot()
+        let integrations = [
+            MockIntegration(number: 1, step: Integration.Step.Completed, sha: "head_sha", result: Integration.Result.Succeeded)
+        ]
+        
+        let actions = SyncPair_PR_Bot.syncPRWithBotIntegrations(pr: pr, bot: bot, integrations: integrations)
+        
+        XCTAssertEqual(actions.integrationsToCancel!.count, 0)
+        XCTAssertFalse(actions.startNewIntegration ?? true)
+        XCTBAssertNotNil(actions.githubStatusToSet)
+        XCTAssertEqual(actions.githubStatusToSet!.status.state, Status.State.Success)
+    }
+    
+    func testTwoIntegrationOneRunningOnePending() {
+        
+        let (pr, bot) = self.mockedPRAndBot()
+        let integrations = [
+            MockIntegration(number: 1, step: Integration.Step.Building, sha: "head_sha"),
+            MockIntegration(number: 2, step: Integration.Step.Pending, sha: "head_sha")
+        ]
+        
+        let actions = SyncPair_PR_Bot.syncPRWithBotIntegrations(pr: pr, bot: bot, integrations: integrations)
+        
+        XCTAssertEqual(actions.integrationsToCancel!.count, 1)
+        XCTAssertFalse(actions.startNewIntegration ?? true)
+        XCTBAssertNotNil(actions.githubStatusToSet)
+        XCTAssertEqual(actions.githubStatusToSet!.status.state, Status.State.Pending)
+    }
+
+    func testTwoIntegrationsDifferentCommits() {
+        
+        let (pr, bot) = self.mockedPRAndBot()
+        let integrations = [
+            MockIntegration(number: 1, step: Integration.Step.Building, sha: "head_sha_old"),
+        ]
+        
+        let actions = SyncPair_PR_Bot.syncPRWithBotIntegrations(pr: pr, bot: bot, integrations: integrations)
+        
+        XCTAssertEqual(actions.integrationsToCancel!.count, 1)
+        XCTAssertTrue(actions.startNewIntegration!)
+        XCTBAssertNil(actions.githubStatusToSet) //no change
+    }
+    
+    //TODO: add more complicated cases
+    
 }
