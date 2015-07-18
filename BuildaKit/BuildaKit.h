11ea16f3b3c81aa23b92535c393012f730d5ef83
Honza Dvorsky <honza@swiftkey.com>
2015-07-18 13:03:50 +0100

    adding BuildaKit to host files for buildasaur's logic (but not the UI)

---
+++ BuildaKit/BuildaKit.h
@@ -0,0 +1,19 @@
+//
+//  BuildaKit.h
+//  BuildaKit
+//
+//  Created by Honza Dvorsky on 18/07/2015.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BuildaKit.
+FOUNDATION_EXPORT double BuildaKitVersionNumber;
+
+//! Project version string for BuildaKit.
+FOUNDATION_EXPORT const unsigned char BuildaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuildaKit/PublicHeader.h>
+
+
