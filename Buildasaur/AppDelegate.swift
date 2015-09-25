5b3fd165e88e6dea5895a06e8d1bebb83189e22d
Honza Dvorsky <czechboy0@gmail.com>
2015-09-25 16:36:35 +0100

    YES! finally got it working. will need to be moved to XcodeServerSDK, naturally, but this is how you can hook into it. took me months to find this one.

--- Buildasaur/AppDelegate.swift
+++ Buildasaur/AppDelegate.swift
@@ -17,15 +17,55 @@ Also, you can find the log at ~/Library/Application Support/Buildasaur/Builda.lo
 import BuildaUtils
 import XcodeServerSDK
 import BuildaKit
+import Alamofire
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSURLSessionDelegate {
     
     let menuItemManager = MenuItemManager()
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         
         self.menuItemManager.setupMenuBarItem()
+        self.startPolling1()
+    }
+    
+    var af: Alamofire.Manager!
+    
+    func timestamp() -> Int {
+        return Int(NSDate().timeIntervalSince1970)*1000
+    }
+    
+    func startPolling1() {
+        let st = ServerTrustPolicyManager(policies: ["localhost": .DisableEvaluation])
+        let af = Alamofire.Manager(serverTrustPolicyManager: st)
+        self.af = af
+        
+        af
+            .request(.GET, "https://localhost/xcode/internal/socket.io/1/?t=\(timestamp)")
+            .responseString { (req, res, result) -> Void in
+                print("Received: \(result)")
+                
+                if let msg = result.value {
+                    //parse object id and polling techniques
+                    let id = msg.componentsSeparatedByString(":").first!
+                    self.poll(id)
+                }
+        }
+    }
+    
+    func poll(id: String) {
+        
+        let timestamp = self.timestamp()
+        let url = "https://localhost/xcode/internal/socket.io/1/xhr-polling/\(id)?t=\(timestamp)"
+        self.af
+            .request(.GET, url)
+            .responseString { (req, res, result) -> Void in
+                if let str = result.value {
+                    print("Received: \(str)")
+                    self.poll(id)
+                }
+        }
     }
 
     func applicationShouldHandleReopen(sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
